fce8f1acc1f9f48b1df338576ad276f72132bd3d
Erick <e.hoogstrate@student.tue.nl>
2021-05-05 14:40:11 +0200

    Updated GUI and grid function

--- gridoutline.m
+++ gridoutline.m
@@ -1,25 +1,13 @@
 clear all; close all; clc;
 %% Load the GUI
-% run('GUI.mlapp')
+run('GUI.mlapp')
 
-%% Matrices
-source = [zeros(14,9)];
-print  = [zeros(14,9)];
+disp(sprintf("First select the desired positions in the GUI (pop-up) and click confirm. \nNext select the Command Window and press any key."))
+pause()
 
-
-% source(12,4) = 1;
-% % source(5,8) = 1;
-% % source(10,3) = 1;
-% source
-print(4,4) = 1;
-% print(9,2) = 1;
-% print(6,6) = 1;
-print
-
-% Settings
+%% Settings
 alpha = 10; % offset from the plate
 beta = 0; % offset from center line of the plate (calibration line) (minus for offset to the source grid)
-
 
 %% Find source position
 [N_row, N_column] = find(source == 1);
@@ -28,7 +16,8 @@ N_column = 9+1-N_column;
 
 % Calculation
 r = sqrt((10*N_row+alpha).^2+(10*(N_column-1)+40-beta).^2)
-theta = atand((10*N_row+alpha)/(10*N_column+40-beta)) 
+theta = atand((10*N_row+alpha)/(10*N_column+40-beta));
+theta = theta(:,1)
 
 return
 %% Find print position
@@ -36,6 +25,7 @@ return
 
 % Calculation
 r = sqrt((10*N_row+alpha).^2+(10*(N_column-1)+40+beta).^2)
-theta = atand((10*N_row+alpha)/(10*N_column+40+beta)) 
+theta = atand((10*N_row+alpha)/(10*N_column+40+beta)); 
+theta = theta(:,1)
 
 
