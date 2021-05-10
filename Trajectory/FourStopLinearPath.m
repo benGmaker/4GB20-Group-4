8a3464108f393e1888e4d7a3d84240027736d752
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2021-05-10 17:33:49 +0200

    added theta to path planning

--- Trajectory/FourStopLinearPath.m
+++ Trajectory/FourStopLinearPath.m
@@ -4,7 +4,7 @@ function path = FourStopLinearPath(startpos,endpos, height)
     pos2 = endpos;
     pos2.D(2) = pos2.D(2) + height;
     
-    path = [LinearPath(startpos, pos1, 200)];
+    path = LinearPath(startpos, pos1, 200);
     path = [path, LinearPath(pos1, pos2, 200)];
     path = [path, LinearPath(pos2, endpos, 200)]; 
 end
