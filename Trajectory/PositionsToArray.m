4d80a18370f6900fe1235d9e7fe9eba3a9042583
F13 <allarddevree2000@gmail.com>
2021-05-12 15:05:19 +0200

    Position to matrix function fix for Z
    forgot to add Z

--- Trajectory/PositionsToArray.m
+++ Trajectory/PositionsToArray.m
@@ -1,11 +1,11 @@
-function array = PositionsToArray(initR,initZ,r_s,theta_s,r_p,theta_p)
+function array = PositionsToArray(initR,initTheta,initZ,r_s,theta_s,r_p,theta_p,z)
     if length(r_s)>=length(r_p)
         r_p(1:length(r_s));
     elseif length(r_s)<=length(r_p)
         r_s(1:length(r_p));
     end
-    array=[initR;initZ];
+    array=[initR;initTheta;initZ];
     for i=1:length(r_s)
-        array=[array,[r_s(i) r_p(i);theta_s(i) theta_p(i)]];
+        array=[array,[r_s(i) r_p(i);theta_s(i) theta_p(i);z z]];
     end
 end
