162d5ce1c7bce86874206fb6f3f05d32bdd2b69b
F13 <allarddevree2000@gmail.com>
2021-05-14 09:44:54 +0200

    Cleaned up the main file and commented it
    Also added a timeseries as output and hopefully it is the last commit of this SSA :)

--- Robot_Arm.m
+++ Robot_Arm.m
@@ -1,86 +1,60 @@
+clear all; close all; clc;
 % General script that runs the robot arm
 
+z_pickup = 24;      % Pickup height of the bolts
+z_moving = 50;      % Safe moving height over the bolts, even with bolt clamped
+ 
+% This needs to be taken over by calibration
+r_init = 200;       % Initial r of the EE
+theta_init = 0;     % Initial theta of the EE
+z_init = 60;        % Initial z of the EE
+
+
  
 
-clear all; close all; clc;
-
- 
-IK = load("iets.mat");
+IK = load("eqn_IK_robotarm.mat");
 addpath("Kinematics")
 addpath("Trajectory")
 
- 
 
 %% Load the GUI
+
+
+
 run('GUI.mlapp')
-
- 
-
 disp(sprintf("First select the desired positions in the GUI (pop-up) and click confirm. \nNext select the Command Window and press any key."))
 pause()
 
- 
-
-%% Settings
+%% Calibration
 % This needs to be automatically adjusted by calibration.
-
- 
 
 alpha = 80; % offset from the plate
 beta = 0; % offset from center line of the plate (calibration line) (minus for offset to the source grid)
 
- 
+% r_init = 200;       % Initial r of the EE
+% theta_init = 0;     % Initial theta of the EE
+% z_init = 60;        % Initial z of the EE
 
-r_init = 240;
-theta_init = 0;
-z_init = 45;
-z_pickup = 24;
-z_moving = 50;
-
- 
-
-%%
+%% GUI conversion to cylindrical coordinate frame
 [r_s,theta_s] = distance_and_angle(source, 'source', alpha, beta);
 [r_p,theta_p] = distance_and_angle(print, 'print', alpha, beta);
 theta_s = deg2rad(theta_s);
 theta_p = deg2rad(theta_p);
 
-
 %% Path
+% In PositionsToArray the outer positions of the path are listed into an
+% array, so the path can be made between these
 CoordinatePath = PositionsToArray(r_init,theta_init,z_init,r_s,theta_s,r_p,theta_p,z_pickup,z_moving);
-[Rx, Rz] = fcn_IK(CoordinatePath(1,:), CoordinatePath(3,:), IK);
-% startPos = ArmPos;
-% startPos.D = [220,40];
-% startPos.theta = 10;
-% startPos = startPos.DtoC;
-% endPos = ArmPos;
-% endPos.D = [210,40];
-% endPos.theta = -10;
-
-% FourStopLinearPath(startPos,endPos,50)
 
 %% Inverse kinematics
 tic
-n = 2048;
-posArray = ArmPos.empty([0,n*(length(Rx)-1)]);
+n = 2048;   % Number of samples per path part
 phiR=[];
-for i=1:length(Rx)-1
-    
-pos(2*(i-1)+1) = ArmPos;
-pos(2*(i-1)+1).phiX = Rx(i);
-pos(2*(i-1)+1).phiZ = Rz(i);
-pos(2*(i-1)+1) = pos(2*(i-1)+1).phiZXtoFullpos(false);
+for i=1:length(CoordinatePath)-1
 
-pos(2*(i-1)+2) = ArmPos;
-pos(2*(i-1)+2).phiX = Rx(i+1);
-pos(2*(i-1)+2).phiZ = Rz(i+1);
-pos(2*(i-1)+2) = pos(2*(i-1)+2).phiZXtoFullpos(false);
-
-
-r = linspace(pos(2*(i-1)+1).D(1), pos(2*(i-1)+2).D(1), n);
-z = linspace(pos(2*(i-1)+1).D(2), pos(2*(i-1)+2).D(2), n);
+r = linspace(CoordinatePath(1,i), CoordinatePath(1,i+1), n);
+z = linspace(CoordinatePath(3,i), CoordinatePath(3,i+1), n);
 phiR=[phiR,linspace(CoordinatePath(2,i), CoordinatePath(2,i+1),n)];
-
 
 for j=1:n
     posArray(n*(i-1)+j) = ArmPos;
@@ -92,14 +66,20 @@ end
 end
 toc
 
-for i=1:(length(Rx)-1)*n
+for i=1:(length(CoordinatePath)-1)*n
     phiX(i) = posArray(i).phiX;
     phiZ(i) = posArray(i).phiZ;
 end
 %% Figuuuur
 figure()
 hold on
-plot(1:(length(Rx)-1)*n,phiX)
-plot(1:(length(Rx)-1)*n,phiZ)
-plot(1:(length(Rx)-1)*n,phiR)
+plot(1:(length(CoordinatePath)-1)*n,phiX)
+plot(1:(length(CoordinatePath)-1)*n,phiZ)
+plot(1:(length(CoordinatePath)-1)*n,phiR)
 legend('phiX','phiZ','phiR')
+
+%% Timeseries
+t=linspace(0,16*(length(CoordinatePath)-1),n*(length(CoordinatePath)-1)); % @128Hz
+ref_X = timeseries([t;phiX]);
+ref_Z = timeseries([t;phiZ]);
+ref_R = timeseries([t;phiR]);
