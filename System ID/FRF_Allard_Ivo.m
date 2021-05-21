a50677fdc0f9fe2b922ce774a9d7c3d429f489a4
Ivo van der Peet <i.j.a.van.der.peet@student.tue.nl>
2021-05-21 18:32:41 +0200

    FRF Allard Ivo V2

--- System ID/FRF_Allard_Ivo.m
+++ System ID/FRF_Allard_Ivo.m
@@ -1,26 +1,68 @@
-clc;
+clc; close all;
+load('WN_10sec_RXZ_IO.mat')
 
 %% Read output
 out_R_dat = out_R.data;
 out_X_dat = out_X.data;
 out_Z_dat = out_Z.data;
 
-%% Turn into iddata object
-R_dat = iddata(out_R_dat,R',Ts);
-X_dat = iddata(out_X_dat,X',Ts);
-Z_dat = iddata(out_Z_dat,Z',Ts);
+%% PSD [Not necessary]
+% window = 1024
+% [pRR,f_R] = pwelch(out_R_dat, 1024, window/2);
+% 
+% figure(1)
+% semilogx(f_R/max(f_R),10*log10(pRR))
+% xlabel('Frequency (kHz)')
+% ylabel('PSD (dB/Hz)')
 
-%% PSD
-[pRR,f_R] = pwelch(out_R_dat, 2048, 1024, 4096);
+%% TF-estimate
+nfft = 1024;
+[H_R, f_R] = tfestimate(R', out_R_dat, hann(nfft), [], nfft, fs);
+[H_X, f_X] = tfestimate(X', out_X_dat, hann(nfft), [], nfft, fs);
+[H_Z, f_Z] = tfestimate(Z', out_Z_dat, hann(nfft), [], nfft, fs);
 
-figure(1)
-plot(f_R,10*log10(pRR).*f_R)
-xlabel('Frequency (Hz)')
-ylabel('PSD (dB)')
+phase_R = rad2deg(angle(H_R));
+phase_X = rad2deg(angle(H_X));
+phase_Z = rad2deg(angle(H_Z));
 
-%% Spectral analysis
-% FRF_R = spa(R_dat, 20480, []);
+for i = 1:length(phase_R)-1
+    if phase_R(i+1) - phase_R(i) >= 300
+        phase_R(i+1:end) = phase_R(i+1:end) - 360;
+    elseif phase_R(i+1) - phase_R(i) <= -300
+        phase_R(i+1:end) = phase_R(i+1:end) + 360;
+    end
+        
+        
+end
 
-%% Visualize in Bode
+%% Automatic figure
 % figure()
-% bode(FRF_R)
+% tfestimate(R', out_R_dat, hann(nfft), [], nfft, fs);
+% figure()
+% tfestimate(X', out_X_dat, hann(nfft), [], nfft, fs);
+% figure()
+% tfestimate(Z', out_Z_dat, hann(nfft), [], nfft, fs);
+
+%% Manual bode plot
+figure()
+tiledlayout(2,1)
+nexttile;
+semilogx(f_R*2*pi, 10*log10(abs(H_R)))
+title('Manual bode from tfestimate')
+ylabel('Magnitude [dB]')
+xlim([1e1,1e4])
+grid on
+
+nexttile;
+semilogx(f_R*2*pi, phase_R)
+xlabel('Frequency [rad/s]')
+ylabel('Phase [deg]')
+ylim([-360,0])
+xlim([1e1,1e4])
+
+grid on
+
+% line([0,1e4], [-180,-180], 'linestyle', '--')
+
+
+
