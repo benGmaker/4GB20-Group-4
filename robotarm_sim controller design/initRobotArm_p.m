bc34795c457761e6866a794a89b39b75bbc53f03
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2021-05-31 15:36:12 +0200

    Test script for computing performance metrics

--- robotarm_sim controller design/initRobotArm_p.m
+++ robotarm_sim controller design/initRobotArm_p.m
@@ -1,17 +1,17 @@
 %% Initialize robotarm
-
+Tsim = 10;
 fs = 2048;
 Ts = 1/fs;
 
 stepX = 0;
 stepZ = 0;
-stepR = 0;
+stepR = 0.5;
 
 nSamplesForVisualization = 200;
 initialAngleKrukX = 0.809091366467325; % [rad]
 initialAngleKrukZ = 0.025113778764355; % [rad]  
 
-t = 1:Ts:10;
+t = 0:Ts:Tsim;
 phiX = zeros(1,length(t)) + initialAngleKrukX + stepX;
 phiZ = zeros(1,length(t)) + initialAngleKrukZ + stepZ;
 phiR = zeros(1,length(t)) + stepR;
