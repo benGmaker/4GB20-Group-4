7f44fc1bb1309ba059a3e43bf3591a129e2b7c8c
Ben Gortemaker <b.f.gortemaker@student.tue.nl>
2021-05-31 15:22:32 +0200

    Init for step response

--- robotarm_sim controller design/initRobotArm_p.m
+++ robotarm_sim controller design/initRobotArm_p.m
@@ -1,12 +1,26 @@
 %% Initialize robotarm
+
 fs = 2048;
 Ts = 1/fs;
 
+stepX = 0;
+stepZ = 0;
+stepR = 0;
+
 nSamplesForVisualization = 200;
-
 initialAngleKrukX = 0.809091366467325; % [rad]
-initialAngleKrukZ = 0.025113778764355; % [rad]
+initialAngleKrukZ = 0.025113778764355; % [rad]  
 
+t = 1:Ts:10;
+phiX = zeros(1,length(t)) + initialAngleKrukX + stepX;
+phiZ = zeros(1,length(t)) + initialAngleKrukZ + stepZ;
+phiR = zeros(1,length(t)) + stepR;
+solenoid =  zeros(1,length(t));
+
+ref_X = timeseries(phiX',t);
+ref_Z = timeseries(phiZ',t);
+ref_R = timeseries(phiR',t);
+ref_solenoid = timeseries(solenoid',t);
 
 controller_R = load('Controllers\Controller_R.mat');
 controller_X = load('Controllers\Controller_X.mat');
