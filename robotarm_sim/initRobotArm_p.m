fdcc60956b7d09892decf9ce15ff1e93e88e0aaa
Erick <e.hoogstrate@student.tue.nl>
2021-05-31 14:57:00 +0200

    Changed path + added controller folder

--- robotarm_sim/initRobotArm_p.m
+++ robotarm_sim/initRobotArm_p.m
@@ -8,6 +8,6 @@ initialAngleKrukX = 0.809091366467325; % [rad]
 initialAngleKrukZ = 0.025113778764355; % [rad]
 
 
-controller_R = load('C:\REPO\Controller_R.mat');
-controller_X = load('C:\REPO\Controller_X.mat');
-controller_Z = load('C:\REPO\Controller_Z.mat');
+controller_R = load('Controllers\Controller_R.mat');
+controller_X = load('Controllers\Controller_X.mat');
+controller_Z = load('Controllers\Controller_Z.mat');
